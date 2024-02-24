09587a60d3089148f6d8fdcc36b8bf0a16adaee7
Thomas Reggi <reggi@github.com>
2024-02-24 18:02:28 -0500

    Update source.sh

--- source.sh
+++ source.sh
@@ -1,3 +1,6 @@
+# brew install arduino-cli
+# brew install esptool
+
 arduino-cli compile --verbose --fqbn m5stack:esp32:m5stack_cardputer ./Launcher/Launcher.ino --output-dir ./support_files
 arduino-cli compile --verbose --fqbn m5stack:esp32:m5stack_cardputer ./StartApp/StartApp.ino  --output-dir ./support_files
 cd ./support_files
